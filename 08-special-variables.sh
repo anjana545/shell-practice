9e95735b2b68289f978014af9800c2cf9809d279
AnjanaAlapati <anjanaalapati@gmail.com>
2025-12-02 18:51:17 +0530

    shell scripting

--- 08-special-variables.sh
+++ 08-special-variables.sh
@@ -3,5 +3,7 @@
 echo "All variables passed to the script: $@"
 echo "All variables passed to the script: $*"
 echo "script name: $0"
-echo "Current Directory: $USER"
+echo "Current Directory: $PWD"
+echo "Who is running this: $USER"
 echo "Home directory of user: $HOME"
+echo "PID of this script: $$"
