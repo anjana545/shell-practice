db3a3fd179510d114955317f35d73e65ec089999
AnjanaAlapati <anjanaalapati@gmail.com>
2025-12-02 18:48:33 +0530

    shell scripting

--- 08-special-variables.sh
+++ 08-special-variables.sh
@@ -2,3 +2,6 @@
 
 echo "All variables passed to the script: $@"
 echo "All variables passed to the script: $*"
+echo "script name: $0"
+echo "Current Directory: $USER"
+echo "Home directory of user: $HOME"
