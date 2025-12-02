98d425c047daef8c9fa3e74b86b292f8360712be
AnjanaAlapati <anjanaalapati@gmail.com>
2025-12-02 14:19:47 +0530

    shell scripting

---
+++ 02-conversation.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Anjana:: Hi Baby, how are you?"
+echo "Baby:: mama, I am fine. How u doing"
+echo "Anjana:: baby, please stay in mama's stomach for 9m"
+echo "Baby:: ok mama, waiting to see you. I will do whatever u talk to me :)"
