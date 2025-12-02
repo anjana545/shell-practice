86a2d583269a9f5d0feffac9d32e7ba3030398b3
AnjanaAlapati <anjanaalapati@gmail.com>
2025-12-02 18:00:08 +0530

    shell scripting

---
+++ 04-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PERSON1=$1
+PERSON2=$2
+
+echo "$PERSON1:: Hi $PERSON2, how are you?"
+echo "$PERSON2:: $PERSON1, I am fine. How u doing"
+echo "$PERSON1:: $PERSON2, please stay in $PERSON1's stomach for 9m"
+echo "$PERSON2:: ok $PERSON1, waiting to see you. I will do whatever u talk to me :)"
