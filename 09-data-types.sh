2d436fdae3b815a65d1a65e86ebabaab08af5561
AnjanaAlapati <anjanaalapati@gmail.com>
2025-12-02 19:10:38 +0530

    shell scripting

--- 09-data-types.sh
+++ 09-data-types.sh
@@ -3,7 +3,12 @@
 #everything in shell is considered as string
 NUMBER1=100
 NUMBER2=200
+NAMe=Devops
 
-SUM=$((NUMBER1+NUMBER2))
+SUM=$((NUMBER1+NUMBER2+$NAME))
 
 echo "Sum is: $SUM"
+
+LEADERS=("modi","CBN", "PK")
+
+echo Leaders: $LEADERS(@)
